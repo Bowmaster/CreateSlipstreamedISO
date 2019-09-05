599a9cb0f33bf2b911970416c548e8c0b02a48b5
Ben J. Lutterbach <blutterb@bbl.blutterbach.com>
2019-09-05 09:31:56 -0400

    Added zip support for Server 2016 and above

--- 02-InstallDrivers.ps1
+++ 02-InstallDrivers.ps1
@@ -2,8 +2,19 @@ $OSVersion = (Get-WmiObject -Class "Win32_OperatingSystem").Version
 
 if ($OSVersion.StartsWith("10.")) {
     Write-Host "Installing drivers for Server 2016" -ForegroundColor Yellow
-    pnputil.exe /add-driver ".\HPE-Gen10Array-11-27-2018\SmartPqi.inf"
-    pnputil.exe /add-driver ".\VMware-Tools-core-10.3.5-10430147\Drivers\pvscsi\Win8\amd64\pvscsi.inf"
+    Copy-Item .\HPE-Gen10Array-11-27-2018.zip C:\
+    New-Item C:\HPE-Gen10Array-11-27-2018 -ItemType Directory | Out-Null
+    Expand-Archive -Path C:\HPE-Gen10Array-11-27-2018.zip -DestinationPath C:\HPE-Gen10Array-11-27-2018 -Force
+
+    Copy-Item .\VMware-Tools-core-10.3.5-10430147.zip C:\
+    New-Item C:\VMware-Tools-core-10.3.5-10430147 -ItemType Directory | Out-Null
+    Expand-Archive -Path C:\VMware-Tools-core-10.3.5-10430147.zip -DestinationPath C:\VMware-Tools-core-10.3.5-10430147 -Force
+
+    pnputil.exe /add-driver "C:\HPE-Gen10Array-11-27-2018\SmartPqi.inf"
+    pnputil.exe /add-driver "C:\VMware-Tools-core-10.3.5-10430147\Drivers\pvscsi\Win8\amd64\pvscsi.inf"
+    
+    Remove-Item -Path "C:\HPE-Gen10Array-11-27-2018.zip","C:\VMware-Tools-core-10.3.5-10430147.zip","C:\HPE-Gen10Array-11-27-2018","C:\VMware-Tools-core-10.3.5-10430147" -Recurse -Force
+
     Write-Host "Finished installing Server 2016 drivers, please check the output below to verify your drivers are installed" -ForegroundColor Green
     pnputil.exe /enum-drivers
 } elseif ($OSVersion.StartsWith("6.1")) {
